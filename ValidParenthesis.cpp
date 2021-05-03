34e8f12ddf44192fdb9a6f589ca8cc4341b53a9f
Aparna Juhi <54853999+AparnaJuhi@users.noreply.github.com>
2021-05-03 16:25:06 +0530

    Valid Parenthesis
    Using stack

---
+++ ValidParenthesis.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(' || s[i]=='[' || s[i]== '{')
+                st.push(s[i]);
+            else
+            {
+                if(st.empty())
+                    return false;
+                if(s[i]==')')
+                {
+                    if(st.top()=='(')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else if(s[i]==']')
+                {
+                    if(st.top()=='[')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else if(s[i]=='}')
+                {
+                    if(st.top()=='{')
+                        st.pop();
+                    else
+                        return false;
+                }
+            }
+        }
+        if(st.empty())
+            return true;
+        return false;
+    }
+int main()
+{
+	string s;
+	cin>>s;
+	cout<<isValid(s);
+}

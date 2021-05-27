3432ab7104831a194cf479c8506d2e6dff23f3b0
Aparna Juhi <54853999+AparnaJuhi@users.noreply.github.com>
2021-05-27 11:20:10 +0530

    Unique Paths

---
+++ Unique_paths.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int numberOfPaths(int m, int n){
+        // code here
+         long long int mat[m][n];
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 || j==0)
+                    mat[i][j]=1;
+                else
+                mat[i][j]=(mat[i-1][j]+mat[i][j-1])%1000000007;
+            }
+        }
+        return mat[m-1][n-1];
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> m >> n;
+        Solution ob;
+        cout<<ob.numberOfPaths(m,n)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends

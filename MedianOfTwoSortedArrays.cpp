d7d802ab1f0fea076e994299786714cd6193c4c7
Aparna Juhi <54853999+AparnaJuhi@users.noreply.github.com>
2021-05-29 12:09:49 +0530

    Median of two sorted arrays- log(n)

--- MedianOfTwoSortedArrays.cpp
+++ MedianOfTwoSortedArrays.cpp
@@ -1,110 +1,126 @@
-//Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int findMedian(int *, int , int *, int);
-
-int main() {
-	int t;
-	cin >> t;
-	
-	while(t--){
-	    int n, m;
-	    cin >> n >> m;
-	    int arr[n];
-	    int brr[m];
-	    
-	    for(int i = 0;i<n;i++){
-	        cin >> arr[i];
-	    }
-	    
-	    for(int i = 0;i<m;i++){
-	        cin >> brr[i];
-	    }
-	    
-	    if(n < m)
-	        cout << findMedian(arr, n, brr, m) << endl;
-	   else
-	        cout << findMedian(brr, m, arr, n) << endl;
-	    
-	}
-
-}// } Driver Code Ends
-
-
-//User function template for C++
-
-// arr : given array with size n 
-// brr : given array with size m
-int findMedian(int arr[], int n, int brr[], int m){
-    int median=(n+m)/2;
-    if((n+m)%2!=0){
-    int count=0;int i=0,j=0;
-    int ans;
-    while(count<=median && i<n &&j<m)
-    {
-        if(arr[i]<brr[j])
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        int n1=nums1.size();
+        int n2=nums2.size();
+         double two=2.00;
+        //time complexity log(m+n)
+        if(n1<=n2)
         {
-            ans=arr[i];
-            count++;i++;
+            //consider nums1
+            int start=0;
+            int end=n1;
+            int max1=INT_MIN,max2=INT_MIN;
+            int min1=INT_MAX, min2=INT_MAX;
+            while(start<=end)
+            {
+                int i1=(start+end)/2; //mid in nums1
+                int i2=((n1+n2+1)/2)-i1;
+                if(i1>0)
+                {
+                    max1=nums1[i1-1];
+                }
+                else
+                {
+                    max1=INT_MIN;
+                }
+                if(i2>0)
+                {
+                    max2=nums2[i2-1];
+                }
+                else
+                {
+                    max2=INT_MIN;
+                }
+                if(i1<n1)
+                {
+                    min1=nums1[i1];
+                }
+                else
+                    min1=INT_MAX;
+                if(i2<n2)
+                    min2=nums2[i2];
+                else
+                    min2=INT_MAX;
+                if(min2>=max1 && min1>=max2)
+                {
+                    //ideal condition
+                    if((n1+n2)%2!=0)
+                        return max(max1,max2);
+                    else
+                        return ((double)((max(max1,max2)+min(min1,min2))/two));
+                }
+                else if(max1>min2)
+                {
+                    end=i1-1;
+                }
+                else
+                {
+                    start=i1+1;
+                }
+                
+            }
+            
         }
         else
         {
-           ans=brr[j];
-            count++; j++;
+            int start=0;
+            int end=n2;
+            int max1=INT_MIN,max2=INT_MIN;
+            int min1=INT_MAX, min2=INT_MAX;
+            while(start<=end)
+            {
+                int i1=(start+end)/2; //mid in nums1
+                int i2=((n1+n2+1)/2)-i1;
+                if(i1!=0)
+                {
+                    max1=nums2[i1-1];
+                }
+                else
+                {
+                    max1=INT_MIN;
+                }
+                if(i2!=0)
+                {
+                    max2=nums1[i2-1];
+                }
+                else
+                {
+                    max2=INT_MIN;
+                }
+                if(i1<n2)
+                {
+                    min1=nums2[i1];
+                }
+                else
+                    min1=INT_MAX;
+                
+                if(i2<n1)
+                    min2=nums1[i2];
+                else
+                    min2=INT_MAX;
+                if(min2>=max1 && min1>=max2)
+                {
+                   
+                    //ideal condition
+                    if((n1+n2)%2!=0)
+                        return (double)max(max1,max2);
+                    else
+                        return ((double)(max(max1,max2)+min(min1,min2))/two);
+                }
+                else if(min1<max2)
+                {
+                    start= i1+1;
+                }
+                else
+                {
+                    end=i1-1;
+                }
+                
+            }
         }
+        return 0;
+            
     }
-    
-     while(count<=median && i<n)
-     {
-          ans=arr[i];
-            count++;i++;
-     }
-     
-     while(count<=median && j<m)
-     {
-         ans=brr[j];
-            count++; j++; 
-     }
-     return ans;
-    
-    }
-    else
-    {
-        //it means we need to output the avg of two
-         int count=0;int i=0,j=0;
-    int ans1=0,ans2=0;
-    while(count<=median && i<n &&j<m)
-    {
-        if(arr[i]<brr[j])
-        {
-            ans1=ans2;
-            ans2=arr[i];
-            count++;i++;
-        }
-        else
-        {
-            ans1=ans2;
-            ans2=brr[j];
-            count++; j++;
-        }
-    }
-    
-     while(count<=median && i<n)
-     {
-            ans1=ans2;
-            ans2=arr[i];
-            count++;i++;
-     }
-     
-     while(count<=median && j<m)
-     {
-            ans1=ans2;
-            ans2=brr[j];
-            count++; j++; 
-     }
-     return((ans1+ans2)/2);
-    }
-
-}
+};

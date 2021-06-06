8d9dba82f7c36fad6211aa8fcd77d9fe3d3bfff7
Aparna Juhi <54853999+AparnaJuhi@users.noreply.github.com>
2021-06-06 09:53:15 +0530

    Longest Increasing Subsequence

---
+++ LongestIncreasingSubsequence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int binarySearch(int a[], int l, int r, int val)
+    {
+        while(l<r)
+        {
+            int mid=(l+r)/2;
+            if(a[mid]>=val)
+            {
+                r=mid;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return r;
+    }
+    int lengthOfLIS(vector<int>& arr) {
+        //make one more array named as tail
+        
+        int n=arr.size();
+        int tail[n];
+        tail[0]=arr[0];
+        int len=1;
+        //to store length
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]>tail[len-1])
+            {
+                tail[len]=arr[i];
+                len++;
+            }
+            else
+            {
+                int idx=binarySearch(tail,0,len,arr[i]);
+                tail[idx]=arr[i];
+            }
+        }
+        return len;
+        
+    }
+};
